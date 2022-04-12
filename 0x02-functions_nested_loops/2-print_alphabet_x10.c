cea073db188bd1bc3d3a61a51f284e6a6addc177
Giggs Bradly <giggsb9@gmail.com>
2022-04-12 11:42:47 -0700

    Done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,11 @@ void print_alphabet_x10(void)
 {
 int i = 0;
 char ch;
-while (i < 10)	
+while(i < 10)	
 {
 ch = 'a';
-while (ch <= 'z')								{
+while(ch <= 'z')		
+{
 _putchar(ch);			 
 ch++;
 }

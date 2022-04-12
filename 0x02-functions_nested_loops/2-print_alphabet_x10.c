4ff971c5eb209fedb12b7688e99c24aad8cb0552
Giggs Bradly <giggsb9@gmail.com>
2022-04-12 10:38:47 -0700

    Done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_alphabet_x10 - prints 10 times alphabet in lowercase
- *   *
- *    * void has no return value
- *     */
+ * print_alphabet_x10 - prints 10 times alphabet in lowercase
+ *
+ * void has no return value
+ */
 
 void print_alphabet_x10(void)
 {

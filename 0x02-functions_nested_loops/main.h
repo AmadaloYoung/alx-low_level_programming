9d599c966250bae30e105dca7d8fdca10cf3853b
Giggs Bradly <giggsb9@gmail.com>
2022-04-12 11:20:00 -0700

    Done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,12 @@ void print_alphabet(void);
 
 /*prints 10 times the alphabet*/
 void print_alphabet_x10(void);
+
+/*checks for lowercase character*/
+int _islower(int c);
+
+/*checks for alphabetic character*/
+int _isalpha(int c);
+
+/*prints the sign of a number*/
+int print_sign(int n);

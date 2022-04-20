ba4ce5022ca458a3dd78763115a8bb0b58135204
Giggs Bradly <giggsb9@gmail.com>
2022-04-20 04:14:34 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
-
-/*
- * reset_to_98 - change n=402 to n=98
- * @n:pointer
- * Return: Void .
- */
+/**
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
+*/
 
 void reset_to_98(int *n)
 {
-	*n = 98;
-}	
+
+*n = 98;
+
+}

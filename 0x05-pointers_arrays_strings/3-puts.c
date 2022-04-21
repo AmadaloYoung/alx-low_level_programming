0ffbab819fedd9721e22397df5c896c0e0630ab4
Giggs Bradly <giggsb9@gmail.com>
2022-04-21 09:41:44 -0700

    Done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,15 +4,13 @@
  * @str: pointer to the string to print
  * Return: void
 */
-
-
 void _puts(char *str)
 {
 int i = 0;
 while (str[i])
 {
-	_putchar(str[i]);
-	i++;
+_putchar(str[i]);
+i++;
 }
 _putchar('\n');
 }

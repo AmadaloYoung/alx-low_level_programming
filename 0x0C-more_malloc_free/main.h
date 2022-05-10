73e0b51609184ca963468330fee523ffbb6d7a75
Giggs Bradly <giggsb9@gmail.com>
2022-05-10 11:41:56 -0700

    Done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+/* prints putchar*/
+int _putchar (char c);
 
-#include <stdlib.h>
-
+/*Function that allocates memory*/
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
-void _puts(char *str);
 
-#endif
+/*function concatenatestwo strings*/
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* function that allocates memory for an array*/
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* function that creates an array of integers*/
+int *array_range(int min, int max);
+
+/* function that reallocates a memory block*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
